76a5a385337ab0485c60c33f80993169a00f0d72
LeeZi <35288214+xianyu21@users.noreply.github.com>
2022-12-19 10:48:12 +0800

    提交代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,13 +21,13 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+    <a href="https:vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
     Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a href="https:github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
